0a78620fef3969ac73266c54892095fe28effcf8
Anfel Achouri <105541670+Anfelachr12@users.noreply.github.com>
2023-06-17 03:53:44 +0100

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,14 @@
 #include<stdio.h>
-/**
- * main - Entry point
- *
- *Description: A C program that prints with put functiom
- *
- * Return: Always 0 (Success)
- */
+
+ /**
+  * main - Entry point
+  *
+  * Description: A C program that prints with put functiom
+  *
+  * Return: Always 0 (Success)
+ */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
+        puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
